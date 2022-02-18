68e97ce9515d5f4414a0fd45c5e5f87696c66838
Steve <callasteven@gmail.com>
2022-02-18 12:27:06 -0700

    Adjust formating in API function

--- main.js
+++ main.js
@@ -573,30 +573,30 @@ function createEmojiBoard(tileEmoji) {
 // SECTION API CODE
 getWebsterDictionaryAPI = () => {
   console.log('a')
-    var elementaryDefinition = '';
-    var collegeDefinition = '';
-    let currentRow = Math.floor(allInput.length / 5);
+  var elementaryDefinition = '';
+  var collegeDefinition = '';
+  let currentRow = Math.floor(allInput.length / 5);
   // if (currentRow === 6) {
-    fetch(`https://www.dictionaryapi.com/api/v3/references/sd2/json/${solution}?key=8a8c06ea-289c-450d-90f1-cf98924da140`) //elementary dictionary
-        .then((response) => response.json())
-        .then(function (definition) {
-          elementaryDefinition = definition[0].shortdef[0];
-          })
-        .catch(err => {
-          console.error("API #1 failed:", err.message);
-        })  
-    fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${solution}?key=d6ad76fd-5324-4925-834b-17a06efafce6`) //college dictionary
-        .then((response) => response.json())
-        .then(function (definition) {
-          collegeDefinition = definition[0].shortdef[0];
-          console.log(definition[0].hwi.prs[0].sound.audio);
-          })
-        .catch(err => {
-          console.error("API #2 failed:", err.message);
-        }) 
-    setTimeout(() => { 
-      displayDefintion(elementaryDefinition, collegeDefinition)
-    }, 1000);
+  fetch(`https://www.dictionaryapi.com/api/v3/references/sd2/json/${solution}?key=8a8c06ea-289c-450d-90f1-cf98924da140`) //elementary dictionary
+      .then((response) => response.json())
+      .then(function (definition) {
+        elementaryDefinition = definition[0].shortdef[0];
+        })
+      .catch(err => {
+        console.error("API #1 failed:", err.message);
+      })  
+  fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${solution}?key=d6ad76fd-5324-4925-834b-17a06efafce6`) //college dictionary
+      .then((response) => response.json())
+      .then(function (definition) {
+        collegeDefinition = definition[0].shortdef[0];
+        console.log(definition[0].hwi.prs[0].sound.audio);
+        })
+      .catch(err => {
+        console.error("API #2 failed:", err.message);
+      }) 
+  setTimeout(() => { 
+    displayDefintion(elementaryDefinition, collegeDefinition)
+  }, 1000);
   // }
 }
 
