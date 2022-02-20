01a280a547d607f0cb6b8d3705f93a8c6631bc5e
Steve <callasteven@gmail.com>
2022-02-20 13:15:11 -0700

    Add lotti player confetti

--- confetti.js
+++ confetti.js
@@ -1,13 +1,23 @@
 
 let animateConfetti = document.querySelector('#confetti');
+let lottiPlayerConfetti = document.querySelector('.confetti-lottiplayer-wrapper');
 
 function createConfetti() {
-    for (let i = 0; i < 20; i++) {
-      animateConfetti.innerHTML += `<div class='confetti'></div>`;
-    }
+  for (let i = 0; i < 20; i++) {
+    animateConfetti.innerHTML += `<div class='confetti'></div>`;
+  }
+  lottiPlayerConfetti.innerHTML = `
+    <lottie-player
+      src="https://assets8.lottiefiles.com/datafiles/U1I3rWEyksM9cCH/data.json"  
+      background="transparent"  
+      speed="1"  
+      style="width: 300px; height: 300px;"
+      loop 
+      autoplay>
+    </lottie-player>`
+    lottiPlayerConfetti.classList.remove('hidden');
   setTimeout(() => {
-    for (let i = 0; i < 20; i++) {
       animateConfetti.innerHTML = ``;
-    }
+      lottiPlayerConfetti.classList.add('hidden');
   }, 5000);
 }
