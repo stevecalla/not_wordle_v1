fb9039aaa8825821cc7f06d0d7ab737ca27c315b
Steve <callasteven@gmail.com>
2022-03-02 19:06:33 -0700

    Add flex row wrap to game board to fix copy function

--- main.js
+++ main.js
@@ -673,8 +673,33 @@ function createGameStatsMenu() {
       <p class='game-board'>${gameBoard}</p>
     </div>
     <button onclick='copyGameBoardButton()'>Copy Game Board</button>
-      `;
+    <input id="btn" onclick="CopyToClipboard('createGamesStatsMenu')" type="button" value="Copy"></input>
+  `;
   // document.getElementById('hamburgerPopupMenu').classList.toggle('hidden');
+
+  // var textContent = document.getElementById('createGamesStatsMenu').innerText;
+  // navigator.clipboard.writeText(textContent).then(()=>{alert(`${textContent}`)});
+}
+
+function CopyToClipboard(element) {
+  var doc = document
+  , text = doc.getElementById(element)
+  , range, selection;
+  
+  if (doc.body.createTextRange) {
+    range = doc.body.createTextRange();
+    range.moveToElementText(text);
+    range.select();
+  } else if (window.getSelection) {
+    selection = window.getSelection();        
+    range = doc.createRange();
+    range.selectNodeContents(createGamesStatsMenu);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
+  document.getElementById("btn").value="Copied";
 }
 
 // SECTION DARK & CONTRAST MODE
