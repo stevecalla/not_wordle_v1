3be93e3fadf38c9ce171e7d5227959206e2d9eda
Steve <callasteven@gmail.com>
2022-03-10 11:25:47 -0700

    Remove focus from hide instrx. Add focus current tile to refresh.

--- main.js
+++ main.js
@@ -159,7 +159,7 @@ function hideInstructions() {
 
   document.getElementById('toggleInstructionsOffIcon').classList.toggle('hidden');
   document.getElementById('toggleInstructionsOnIcon').classList.toggle('hidden');
-  document.getElementById('id0').focus();
+  // document.getElementById('id0').focus();
   // document.getElementById('id0').blur();
   // focusCurrentTile();
   document.getElementById('hamburgerPopupMenu').classList.add('hidden');
@@ -855,7 +855,7 @@ function getKeyboardButton() {
 
 function refreshButton() {
   location.reload(true);
-  // focusCurrentTile();
+  focusCurrentTile();
 }
 
 function toggleDarkModeButton() {
