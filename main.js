658b5171636be29ce32c635973cbba77e1ee4c47
Steve <callasteven@gmail.com>
2022-03-12 17:06:13 -0700

    Adjust margins for history board

--- main.js
+++ main.js
@@ -1421,6 +1421,7 @@ function createHistoryBoard() {
 
 function hideHistoryBoard() {
   document.getElementById('historyBoardWrapper').classList.toggle('hidden');
+  document.getElementById('historyTable').classList.remove('hidden');
 }
 
 // function hideHistoryTable() {
