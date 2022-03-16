aa5cf0cf84dd28f04db1ad75b66bdf6f3be26921
Steve <callasteven@gmail.com>
2022-03-16 17:43:06 -0600

    Add copy button to history table

--- main.js
+++ main.js
@@ -125,7 +125,7 @@ function loadTasks() {
   toggleContrastMode(gameStats.contrastMode);
   createGameTiles();
 
-  createHistoryTable();
+  // createHistoryTable();
   // document.getElementById('tableHeaderRow').classList.remove('hidden');
   // focusCurrentTile();
   // let currentElement = document.getElementById('id0');
@@ -1112,9 +1112,11 @@ function copyToClipboard(element) {
   document.getElementById('clickToHideX') ? document.getElementById('clickToHideX').classList.add('hidden') : null;
   element !== 'historyTable' ? document.getElementById('historyTable').classList.add('hidden') : null;
   document.getElementById('historyTable') ? document.getElementById('tableHeaderRow').classList.add('hidden') : null;
+  element === 'historyTable' ? document.getElementById('historyButton').classList.add('hidden') : null;
   document.execCommand('copy');
   document.getElementById('statsButtons') ? document.getElementById('statsButtons').classList.remove('hidden') : null;
   document.getElementById('clickToHideX') ? document.getElementById('clickToHideX').classList.remove('hidden') : null;
+  element === 'historyTable' ? document.getElementById('historyButton').classList.remove('hidden') : null;
   // document.getElementById('statsButtons').classList.remove('hidden');
   // document.getElementById('clickToHideX').classList.remove('hidden');
 
@@ -1314,7 +1316,7 @@ function createHistoryTable() {
       <th>${gameStats.gameCount}</th>
       <th>${winCountPercent}</th>
       <th></th>
-      <th><button onclick="copyToClipboard('historyTable')">Copy</button></th>
+      <th><button id='historyButton' onclick="copyToClipboard('historyTable')">Copy</button></th>
     </tr>
   `
   historyTableStyle();
