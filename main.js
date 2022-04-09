aa49960145346b1301881a8305dc8edf312f9ff0
Steve Calla <callasteven@gmail.com>
2022-04-09 15:09:27 -0600

    Adjust play sound formatting

--- main.js
+++ main.js
@@ -885,6 +885,7 @@ function displayDefintion(elementaryDefinition, collegeDefinition, word) {
   document.querySelector('#definition').innerHTML = `
     Definition: ${displayDefinition}
     <div class="play-sound-wrapper">
+      <svg class='play-button' onclick='textToSpeech(displayDefinition)' version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 386.972 386.972"  xml:space="preserve"><path class='play-button' d="M25.99,0v386.972l334.991-193.486L25.99,0z M55.99,51.972l245.009,141.514L55.99,335V51.972z"/><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
       <p class='play-button' onclick='textToSpeech(displayDefinition)'>Play</p>
       <p class='play-button-line'>|</p>
       <p class='play-button'>0:02</p>
@@ -894,6 +895,7 @@ function displayDefintion(elementaryDefinition, collegeDefinition, word) {
   displaySolution.innerHTML = `
       <div>${solution.join('')}</div>
       <div class="play-sound-wrapper">
+        <svg class='play-button' onclick='textToSpeech(solutionText)' version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 386.972 386.972"  xml:space="preserve"><path class='play-button' d="M25.99,0v386.972l334.991-193.486L25.99,0z M55.99,51.972l245.009,141.514L55.99,335V51.972z"/><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>
         <p class='play-button' onclick='textToSpeech(solutionText)'>Play</p>
         <p class='play-button-line'>|</p>
         <p class='play-button'>0:02</p>
